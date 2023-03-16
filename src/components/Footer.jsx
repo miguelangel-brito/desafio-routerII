4135aaa4d2e15ae035bd883a73968be456d23684
dicuru.arq <dicuru.arq@dicuruarq.local>
2023-03-16 16:47:24 -0300

    commit global router__

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../assets/css/navbar.css";
+
+const Footer = () => {
+
+    return ( 
+        <footer className=" text-center text-light p-2 footer">
+            <div className="d-flex">
+                <p className="p-2 flex-grow-1 fs-4">Miguel Brito © 2023. Todos los derechos reservados.</p>
+                </div>
+            </footer>
+    );
+}
+export default Footer;
